2812cbc425c4831dbc41b89bf35bdc65896db324
Noël Bossart <noel.bossart@namics.com>
2012-10-13 18:27:36 +0200

    Updated CTRL for event

--- ext_tables.php
+++ ext_tables.php
@@ -70,7 +70,9 @@ t3lib_extMgm::allowTableOnStandardPages('tx_nboevents_domain_model_event');
 $TCA['tx_nboevents_domain_model_event'] = array(
 	'ctrl' => array(
 		'title'	=> 'LLL:EXT:nboevents/Resources/Private/Language/locallang_db.xml:tx_nboevents_domain_model_event',
-		'label' => 'title',
+		'label' => 'course',
+		'label_alt' => 'date',
+		'label_alt_force' => true,
 		'tstamp' => 'tstamp',
 		'crdate' => 'crdate',
 		'cruser_id' => 'cruser_id',
