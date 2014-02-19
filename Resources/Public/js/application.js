de18c3176c8648ba2ff401112c8b4d87d6c45c82
Noel Bossart <noel.bossart@namics.com>
2014-02-19 21:37:18 +0100

    Removed gmaps, fixed mod

--- Resources/Public/js/application.js
+++ Resources/Public/js/application.js
@@ -4,6 +4,11 @@
 ;
 (function($){
 	$(document).ready(function(){
+		var $mod = $('.tx-nboevents');
+
+		if($mod.length < 1){
+			return;
+		}
 
 		var $templ = $('.template');
 		$('.data-pool [id]').each(function(){
@@ -21,51 +26,6 @@
 		$('tr[data-href]', $mod).click(function(){
 			window.location = $(this).attr('data-href');
 		});
-		// google maps
-		$('.gmap',$mod).each(function(){
-			var that = this;
-			var $t = $(this);
-
-			var myOptions = {
-				zoom: 6,
-				center: new google.maps.LatLng(47.478232, 9.047241), // Bissfest
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			}
-
-			var search = {
-				'address': $t.attr('data-address').replace(/\r?\n|\r/g, ", "),
-				region: 'sgg'
-			};
-
-			var address = $t.html();
-			var map = new google.maps.Map(that, myOptions);
-			var gc  = geocoder = new google.maps.Geocoder();
-			var infowindow = new google.maps.InfoWindow();
-			var marker;
-
-			gc.geocode( search , function(results, status) {
-				if (status == google.maps.GeocoderStatus.OK) {
-					marker = new google.maps.Marker({
-						map: map,
-						position: results[0].geometry.location
-					});
-					map.setZoom($t.attr('data-zoom')*1);
-					map.setCenter(results[0].geometry.location);
-					if($t.attr('data-infowindow')){
-						infowindow.setContent(address);
-						infowindow.open(map, marker);
-					}
-				} else {
-					//console.log([results,status]);
-				}
-			});
-		});
-
-		var $mod = $('.tx-nboevents');
-
-		if($mod.length < 1){
-			return;
-		}
 
 		// toggle checkbox
 		$('[data-toggle]',$mod).click(function(e){
