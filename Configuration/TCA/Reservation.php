5d222be83471182c9f14b7859a98fc341d611e28
Noel Bossart <noel@Leon.local>
2014-07-04 10:38:52 +0200

    Reverted reservation

--- Configuration/TCA/Reservation.php
+++ Configuration/TCA/Reservation.php
@@ -206,14 +206,25 @@ $TCA['tx_nboevents_domain_model_reservation'] = array(
 			'exclude' => 0,
 			'label' => 'LLL:EXT:nboevents/Resources/Private/Language/locallang_db.xml:tx_nboevents_domain_model_reservation.person',
 			'config' => array(
-				'type' => 'inline',
+				'type' => 'select',
 				'foreign_table' => 'tx_nboevents_domain_model_person',
 				'foreign_table_where' => 'ORDER BY tx_nboevents_domain_model_person.lastname,tx_nboevents_domain_model_person.firstname',
 				'minitems' => 1,
 				'maxitems' => 1,
-				'appearance' => array(
-					'collapseAll' => 1,
-					'expandSingle' => 1,
+				'wizards' => array(
+					'_VALIGN' => 'bottom',
+					'_POSITION' => 'bottom',
+					'suggest' => array(
+						'type' => 'suggest',
+					),
+					'edit' => array(
+						'type' => 'popup',
+						'title' => 'Edit',
+						'script' => 'wizard_edit.php',
+						'icon' => 'edit2.gif',
+						'popup_onlyOpenIfSelected' => 1,
+						'JSopenParams' => 'height=350,width=580,orderstatus=0,menubar=0,scrollbars=1',
+					),
 				),
 			),
 		),
