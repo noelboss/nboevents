3bbb5f6c0179a4b3bf0fbd8e42a89006e4cd6aed
Noël Bossart <noel.bossart@namics.com>
2015-01-22 19:22:45 +0100

    Updated language

--- Classes/Domain/Model/Event.php
+++ Classes/Domain/Model/Event.php
@@ -463,7 +463,8 @@ class Tx_Nboevents_Domain_Model_Event extends Tx_Extbase_DomainObject_AbstractEn
 		$event = $repo->findAllByUid($uid);
 		if($event){
 			$course = count($event->getCourse()) > 0 ? $event->getCourse()->getTitle() : 'Kein Kurs';
-			$label = $event->getEventnr().' – '.$event->getDate()->format('d.m.Y G:i – D').' – '.$course.' – '.$event->getRemaining();
+			setlocale(LC_ALL, 'de_CH.utf-8');
+			$label = $event->getEventnr().' – '.strftime("%a %d.%m.%y – %H:%m",$event->getDate()->getTimestamp()).' – '.$event->getRemaining();
 		}
 		$return['title'] = $label;
 	}
